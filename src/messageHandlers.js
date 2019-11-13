ac099e8fe5c3b0394be51dabc78d5eb6f991f290
dazulu <definitive@outlook.com>
2019-11-13 22:41:05 +0100

    feat(docs): add inline comments

--- src/messageHandlers.js
+++ src/messageHandlers.js
@@ -2,10 +2,13 @@ export default client =>
   class MessageHandlerClass {
     constructor() {
       this.messageHandler = (channel, tags, message, self) => {
+        // We don't want to act on our own messages
         if (self) return;
 
-        let msg = message.trim();
+        // Remove white space from user's message
+        const msg = message.trim();
 
+        // Respond to a command
         if (msg.toLowerCase() === "!beets") {
           client.say(channel, `@${tags.username}! Non-stop beets!`);
         }
