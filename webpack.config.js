ff29f56ffafbc8007d084b0e3a8089da9d6022f1
dazulu <definitive@outlook.com>
2019-11-17 16:31:10 +0100

    style: linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   // Use prod optimisations
-  mode: "production",
+  mode: 'production',
 
   // To target node environment
-  target: "node",
+  target: 'node',
 
   // App entry point for compilation target
-  entry: "./src/index.js",
+  entry: './src/index.js',
 
   // Target for compiled bundle
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
 
   // Including bufferutil & utf-8-validate removes superfluous warnings
-  externals: ["bufferutil", "utf-8-validate"]
+  externals: ['bufferutil', 'utf-8-validate']
 };
