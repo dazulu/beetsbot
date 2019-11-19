594e7f28508edac70d3c28ba361bb1255037a6cf
dazulu <definitive@outlook.com>
2019-11-19 21:26:32 +0100

    feat(!roll): add feature to roll d8, d20, dx

--- src/roll/index.js
+++ src/roll/index.js
@@ -6,14 +6,18 @@ const prefix = config.general.commandPrefix;
 
 export default function(channelName, msg) {
   // pattern to test if a custom number was requested
-  const regex = new RegExp(`^${prefix}roll\\s[\\d]+$`);
+  const regexNumber = new RegExp(`^${prefix}roll\\s[\\d]+$`);
+  const regexDice = new RegExp(`^${prefix}roll\\sd[\\d]+$`);
 
   if (msg === `${prefix}roll`) {
-    client.action(channelName, `rolls ${randNum(100)}`);
-  } else if (regex.test(msg)) {
+    client.action(channelName, `rolls ${randNum(100)} out of 100`);
+  } else if (regexNumber.test(msg)) {
     const max = parseInt(msg.split(' ')[1], 10);
     if (max > 0) {
-      client.action(channelName, `rolls ${randNum(max)}`);
+      client.action(channelName, `rolls ${randNum(max)} out of ${max}`);
     }
+  } else if (regexDice.test(msg)) {
+    const max = parseInt(msg.split('d')[1], 10);
+    client.action(channelName, `rolls a d${max} for ${randNum(max)}`);
   }
 }
